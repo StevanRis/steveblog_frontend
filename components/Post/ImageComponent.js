99fd00dcf592e051082a3255b23926f47d6d0920
StevanRis <affiliate.blogovi@gmail.com>
2022-05-04 04:42:30 +0200

    add figcaption for unsplash and pexel images

--- components/Post/ImageComponent.js
+++ components/Post/ImageComponent.js
@@ -18,7 +18,18 @@ export default function ImageComponent(props) {
         layout="intrinsic"
         alt={props.node.altText}
       />
-      {props.node.caption && <figcaption>{props.node.caption}</figcaption>}
+      {props.node.metadata?.creditLine && (
+        <figcaption className="text-slate-400">
+          {`${props.node.metadata?.creditLine}${
+            props.node.metadata?.source?.name === "unsplash"
+              ? "  -  " + props.node.metadata?.source.url
+              : ""
+          }`}
+        </figcaption>
+      )}
+      {props.node.caption && (
+        <figcaption className="text-black">{props.node.caption}</figcaption>
+      )}
     </figure>
   );
 }
